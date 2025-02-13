4b9d81eaef12e9df24e33ba5c803362aa2e033e4
Gabriel-Manzano <gabriel.mzn.r.xx66@gmail.com>
2025-02-12 19:38:59 -0600

    Commit practica 2
    Acabada

--- FAST/main.py
+++ FAST/main.py
@@ -42,3 +42,12 @@ def actualizarUsuarios(id:int,usuarioActualizado:dict):
             usuarios[index].update(usuarioActualizado)
             return usuarios[index]
     raise HTTPException(status_code=400, detail="El usuario no existe")
+
+#Endponit Eliminar
+@app.delete('/usuarios/{id}', tags=['Operaciones CRUD'])
+def eliminarUsuarios(id:int):
+    for index, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            usuarios.pop(index)
+            return {"Los usuarios existentes restantes son": usuarios}
+    raise HTTPException(status_code=400, detail="El usuario no existe")
