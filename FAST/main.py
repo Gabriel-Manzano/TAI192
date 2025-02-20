6e065be2bef3b7291e3aed737aad350268d496eb
Gabriel-Manzano <gabriel.mzn.r.xx66@gmail.com>
2025-02-20 17:19:33 -0600

    Commit - Avance Práctica 5
    Validaciones

--- FAST/main.py
+++ FAST/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,HTTPException
-from typing import Optional
+from typing import Optional,List
+from pydantic import BaseModel
 
 app= FastAPI(
     title= 'Mi primerAPI 192',
@@ -7,24 +8,32 @@ app= FastAPI(
     version= '1.0.1'
 )
 
+# Modelo de validaciones
+class modeloUsuario(BaseModel):
+    id:int
+    nombre:str
+    edad:int
+    correo:str
+
+# BD ficticia
 usuarios=[
-    {"id": 1,"nombre":"ivan", "edad":37},
-    {"id": 2, "nombre":"isay", "edad":15},
-    {"id": 3, "nombre":"luis", "edad":18},
-    {"id": 4, "nombre":"ana", "edad":37}
+    {"id": 1,"nombre":"ivan", "edad":37, "correo":"ivan@gmail.com"},
+    {"id": 2, "nombre":"isay", "edad":15, "correo":"isay@gmail.com"},
+    {"id": 3, "nombre":"luis", "edad":18, "correo":"luis@gmail.com"},
+    {"id": 4, "nombre":"ana", "edad":37, "correo":"ana@gmail.com"}
 ]
 
-#Endponit home
+# Endponit home
 @app.get('/', tags=['Hola Mundo'])
 def home():
     return {'hello':'Hello World'}
 
-#Endponit CONSULTA TODOS
-@app.get('/TodosUsuarios', tags=['Operaciones CRUD'])
+# Endponit CONSULTA TODOS
+@app.get('/TodosUsuarios', response_model= List[modeloUsuario], tags=['Operaciones CRUD'])
 def leerUsuarios():
     return {"Los usuarios registrados son ": usuarios}
 
-#Endponit Agregar nuevos
+# Endponit Agregar nuevos
 @app.post('/usuarios/', tags=['Operaciones CRUD'])
 def agregarUsuarios(usuario:dict):
     for usr in usuarios:
@@ -34,7 +43,7 @@ def agregarUsuarios(usuario:dict):
     usuarios.append(usuario)
     return usuario
 
-#Endponit Actualizar
+# Endponit Actualizar
 @app.put('/usuarios/{id}', tags=['Operaciones CRUD'])
 def actualizarUsuarios(id:int,usuarioActualizado:dict):
     for index, usr in enumerate(usuarios):
@@ -43,7 +52,7 @@ def actualizarUsuarios(id:int,usuarioActualizado:dict):
             return usuarios[index]
     raise HTTPException(status_code=400, detail="El usuario no existe")
 
-#Endponit Eliminar
+# Endponit Eliminar
 @app.delete('/usuarios/{id}', tags=['Operaciones CRUD'])
 def eliminarUsuarios(id:int):
     for index, usr in enumerate(usuarios):
