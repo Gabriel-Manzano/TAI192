9f08b1a43f73e133b7efb7159cf60c5ba0749b99
Gabriel-Manzano <gabriel.mzn.r.xx66@gmail.com>
2025-02-27 17:20:26 -0600

    Practica 6 - Avance
    GenToken

--- FAST/main.py
+++ FAST/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI,HTTPException
+from fastapi.responses import JSONResponse
 from typing import Optional,List
-from models import modeloUsuario
+from modelsPydantic import modeloUsuario, modeloAuth
+from genToken import createToken
+
 
 app= FastAPI(
     title= 'Mi primerAPI 192',
@@ -20,6 +23,16 @@ usuarios=[
 @app.get('/', tags=['Hola Mundo'])
 def home():
     return {'hello':'Hello World'}
+
+# Endpoitn de autentificación
+@app.post('/auth', tags=['Autentificación'])
+def login(autorizacion:modeloAuth):
+    if autorizacion.email == 'gabriel.mzn.r.xx66@example.com' and autorizacion.passw =='123456789':
+        token:str = createToken(autorizacion.model_dump())
+        print(token)
+        return JSONResponse(content= token)
+    else:
+        return{"Aviso: Usuario no autorizado"}
 
 # Endponit CONSULTA TODOS
 @app.get('/TodosUsuarios', response_model= List[modeloUsuario], tags=['Operaciones CRUD'])
--- FAST/modelsPydantic.py
+++ FAST/modelsPydantic.py
@@ -5,3 +5,8 @@ class modeloUsuario(BaseModel):
     nombre:str = Field(...,min_length=3, max_length=85, description="Solo letras: min 3 max 85")
     edad:int = Field(...,gt=0,lt=125)
     correo:str = Field(..., example="gabriel.mzn.xx66@gmail.com", pattern=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+
+
+class modeloAuth(BaseModel):
+    email:str = Field(..., example="gabriel.mzn.xx66@gmail.com", pattern=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    passw: str = Field(..., min_length=8, strip_whitespace=True, description="Contraseña mínmo 8 caracteres")
